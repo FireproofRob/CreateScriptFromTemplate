ccbf926318bda8fd741a9d8213a97a5ca479b8d3
Rob Dodd <rob@fireproofstudios.com>
2017-01-18 16:55:40 +0000

    Initial commit

---
+++ CreateScriptFromTemplate.cs
@@ -0,0 +1,265 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Linq.Expressions;
+
+public class CreateScriptFromTemplate : EditorWindow
+{
+    [MenuItem("Assets/Create/Script From Template...", false, 50)]
+    public static void CreateEditorScript()
+    {
+        CreateScriptFromTemplate window = GetWindow<CreateScriptFromTemplate>();
+        window.CreatePath = window.GetProjectBrowserPath();
+        window.GatherTemplates();
+    }
+
+    private class TemplateInfo
+    {
+        public string MenuName;
+
+        //Things where the position is specified in the file, and the
+        //value is exposed to the UI (e.g. classname).
+        //Formatted as ##key##
+        public Dictionary<string, string> Replacements;
+
+        //Things where the key and value are specified in the template.
+        //e.g. the extension. 
+        //Formatted as &&key=value&&
+        public Dictionary<string, string> SpecialKeys;
+
+        //The complete contents of the file
+        public string WholeTemplate;
+
+        public int Priority;
+    }
+
+    private Dictionary<string, TemplateInfo> Templates;
+
+    private string[] MenuItems;
+
+    private string CreatePath;
+
+
+    private int Selected = 0;
+
+    private string GetProjectBrowserPath()
+    {
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        return path;
+    }
+
+    void OnGUI()
+    {
+        if (Templates == null || MenuItems == null)
+        {
+            GatherTemplates();
+        }
+
+        TemplateInfo info = Templates[MenuItems[Selected]];
+
+        EditorGUILayout.BeginVertical();
+        Selected = EditorGUILayout.Popup("Template", Selected, MenuItems, GUILayout.Width(350));
+
+        List<string> keys = new List<string>(info.Replacements.Keys);    //Dictionaries don't like it when you make changes in a loop, so cache the key names
+        foreach (string key in keys)
+        {
+            if (key == "Year")
+            {
+                continue;
+            }
+
+            info.Replacements[key] = EditorGUILayout.TextField(key, info.Replacements[key], GUILayout.Width(350));
+        }
+
+        GUILayout.Label("Creating file " + CreatePath + "/" + info.Replacements["ClassName"] + info.SpecialKeys["EXTENSION"]);
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Close"))
+        {
+            Close();
+        }
+
+        EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(info.Replacements["ClassName"]));
+        if (GUILayout.Button("OK"))
+        {
+            CreateScript(info);
+            Close();
+            Templates = null;
+            MenuItems = null;
+        }
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+
+    }
+
+    private void GatherTemplates()
+    {
+        List<string> paths = new List<string>();
+
+        string[] dirs = Directory.GetDirectories(Application.dataPath, "ScriptTemplates", SearchOption.AllDirectories);
+
+        foreach (string dir in dirs)
+        {
+            paths.AddRange(Directory.GetFiles(dir, "*.fpst"));
+        }
+
+        Templates = new Dictionary<string, TemplateInfo>();
+
+        foreach (string template in paths)
+        {
+            TemplateInfo info = ParseTemplate(template);
+
+            if (info != null)
+            {
+                Templates.Add(info.MenuName, info);
+            }
+        }
+
+        //Collect the names to use in the GUI
+        MenuItems = Templates.Keys.ToArray();
+
+        //Sort by priority
+        MenuItems = MenuItems.OrderByDescending(s => Templates[s].Priority).ToArray();
+    }
+
+    private TemplateInfo ParseTemplate(string path)
+    {
+        TemplateInfo temp = new TemplateInfo();
+
+        string template = System.IO.File.ReadAllText(path);
+
+        if (string.IsNullOrEmpty(template))
+        {
+            return null;
+        }
+
+        //Extract the SpecialKeys
+        //https://www.debuggex.com/ is your friend
+        string pattern = @"&&(\w+) *= *(.?[\w/# ]+)&&\n?";
+
+        Regex reg = new Regex(pattern, RegexOptions.Multiline);
+        Match match = reg.Match(template);
+
+        temp.SpecialKeys = new Dictionary<string, string>();
+
+        while (match.Success)
+        {
+            string key = match.Groups[1].Value.ToUpper();
+            string value = match.Groups[2].Value;
+
+            if (!temp.SpecialKeys.ContainsKey(key))
+            {
+                temp.SpecialKeys.Add(key, value);
+            }
+
+            //Remove from the file
+            template = template.Replace(match.Groups[0].Value, "");
+
+            match = match.NextMatch();
+        }
+
+        //Assume .cs if no extension is specified
+        if (!temp.SpecialKeys.ContainsKey("EXTENSION"))
+        {
+            temp.SpecialKeys.Add("EXTENSION", ".cs");
+        }
+
+        //Extract the Replacements
+        pattern = @"##(\w+)##";
+
+        reg = new Regex(pattern, RegexOptions.Multiline);
+        match = reg.Match(template);
+
+        temp.Replacements = new Dictionary<string, string>();
+
+        //Classname is used to name the file, so make sure it's present
+        temp.Replacements.Add("ClassName", "");
+        while (match.Success)
+        {
+            string key = match.Groups[1].Value;
+
+            //Ignore the "Year". That's a special one that'll get filled in automatically
+            if (!temp.Replacements.ContainsKey(key))
+            {
+                temp.Replacements.Add(key, "");
+
+                //Fill in the year now (it's skipped in the GUI)
+                if (key == "Year")
+                {
+                    temp.Replacements[key] = System.DateTime.Now.Year.ToString();
+                }
+            }
+
+            match = match.NextMatch();
+        }
+
+        //If we didn't get a "MenuName" from the special keys, 
+        //Automatically create it from the filename
+        if (!temp.SpecialKeys.TryGetValue("MENUNAME", out temp.MenuName))
+        {
+            temp.MenuName = Path.GetFileNameWithoutExtension(path);
+        }
+
+        temp.Priority = 0;
+        string priorityString;
+        if (temp.SpecialKeys.TryGetValue("PRIORITY", out priorityString))
+        {
+            int priority;
+            if (int.TryParse(priorityString, out priority))
+            {
+                temp.Priority = priority;
+            }
+        }
+        
+
+        //Store the whole Text
+        temp.WholeTemplate = template;
+
+        return temp;
+    }
+
+    private void CreateScript(TemplateInfo info)
+    {
+        //Just in case someone's put .cs at the end of classname, delete it
+        string className = System.IO.Path.GetFileNameWithoutExtension(info.Replacements["ClassName"]);
+
+        string template = info.WholeTemplate;
+
+        string ext = info.SpecialKeys["EXTENSION"];
+
+        foreach (KeyValuePair<string, string> pairs in info.Replacements)
+        {
+            template = template.Replace("##" + pairs.Key + "##", pairs.Value);
+        }
+
+        string finalPath = System.IO.Path.Combine(CreatePath, className + ext.ToLower());
+
+        if (System.IO.File.Exists(finalPath))
+        {
+            Debug.LogError("File already exists: " + finalPath);
+        }
+        else
+        {
+            System.IO.File.WriteAllText(finalPath, template);
+            AssetDatabase.Refresh();
+
+            Object asset = AssetDatabase.LoadAssetAtPath(finalPath, typeof(TextAsset));
+            Selection.activeObject = asset;
+            AssetDatabase.OpenAsset(asset);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # CreateScriptFromTemplate
-Unity utility to make creating new scripts more flexible
+
+A Unity Editor utility providing a more flexible system for creating new scripts (or any other sort of text file) than Unity's built-in "Create->C# Script" option.
+
+##usage
+Copy into an Editor folder somewhere in your Unity project. Use it by right-clicking somewhere in your project view and selecting "Create->Script From Template...". That'll open a dialogue box which allows you which template you want to use.
+
+There's a few standard templates defined (which you should modify to suit your needs).
+
+You can easily create new templates by duplicating the existing ones. Templates can live in any folder called "ScriptTemplates", and must have an extension .fpst. At Fireproof, we have a bunch of them inside our Common folder (which is shared between projects), and some specific to each project.
+
+###Example Template
+Here's an example template:
+
+```C#
+&&MenuName=C#/Editor/CustomInspector&&
+&&Priority=2&&
+//
+// ##ClassName##.cs
+// Copyright (c) ##Year## Fireproof Studios, All Rights Reserved
+//
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(##EditorTargetClass##))]
+public class ##ClassName## : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        ##EditorTargetClass## myTarget = (##EditorTargetClass##)target;
+        
+        DrawDefaultInspector();
+    }
+}
+```
+
+There's two sorts of special tokens:
+    "&&" Is a special token to tell the system how the template should be presented. They're stripped out of the final script.
+    "##" Is something that should be replaced in the script by something that the user will specify in the UI
+
+Let's see how those are used in the example:
+`&&MenuName=C#/Editor/CustomInspector&&` Tells the system where to put the template in the dialogue box's dropdown of templates
+`&&Priority=1&&` Priority can be used to force often-used options to the top of the list (We use this for Monobehaviours)
+`##ClassName##` Tells the system to show a "Classname" text-entry box in the dialogue. When the script is created, all instances of this will be replaced by whatever was entered. Note that Classname is a special case - the system uses this as the filename (combined with the &&EXTENSION&& property)
+`##Year##` This is a special replacement that the system knows about. It'll insert the current year without presenting it in the dialogue
+`##EditorTargetClass##` A "normal" replacement. The system knows nothing about what "EditorTargetClass" means, and will just blindly replace whatever the user enters into the dialogue.
+
+You can define as many extra ##Token## things as you like. They'll all be presented in the UI.
+
+The only other special token is "Extension". By default, the created script will use the ##ClassName## token with a ".cs" extension. If you want something else, put a like like `&&EXTENSION=.shader&&` at the top of the file.
